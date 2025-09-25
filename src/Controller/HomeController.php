9a944725c1305c99e59404e970fcb4f9c81c7642
bensaadasala <bensaadasala126@gmail.com>
2025-09-25 13:52:52 +0100

    Ajout des derniers travaux du projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,44 +8,47 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')] // pour execution  path /home.., 
-    public function index(): Response //
+    #[Route('/home', name: 'home_page')] // ✅ name مبدّل
+    public function index(): Response
     {
-        return $this->render('home/index.html.twig', [ // reder men absreact conroler win paht=templet mta3 conroler jdid 
-            'controller_name' => 'HomeController',// varable hat feha chene 
-            'identifiant'=>5 
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'identifiant' => 5,
         ]);
     }
-    #[Route('/hello', name: 'hello')] 
-    public function hello():Response{
-        return new Response(content:"Hello 3a26");
 
+    #[Route('/hello', name: 'hello')]
+    public function hello(): Response
+    {
+        return new Response("Hello 3a26"); // ✅ صياغة Response صحيحة
     }
 
-#[Route('/contact/{tel}', name: 'contact')] // kol wahda 3andha name wahdaha 
-    public function contact($tel):Response{
-        return $this->render('home/contact.html.twig',['telephone'=>$tel]);
-
-    }
- #[Route('/show', name: 'show')] 
-    public function show():Response{
-        return new Response(content:"bienvenuse a ooredoooo");
-
+    #[Route('/contact/{tel}', name: 'contact')]
+    public function contact(string $tel): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'telephone' => $tel,
+        ]);
     }
 
+    #[Route('/show', name: 'show')]
+    public function show(): Response
+    {
+        return new Response("bienvenue à Ooredoo");
+    }
 
-    #[Route('/affiche', name: 'affiche')] // kol wahda 3andha name wahdaha 
-    public function affiche():Response{
+    #[Route('/affiche', name: 'affiche')]
+    public function affiche(): Response
+    {
         return $this->render('home/apropos.html.twig');
-
     }
 
-
-
-       #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'homepage')] // ✅ name مبدّل
     public function rediriger(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'Redirection depuis /',
+            'identifiant' => 999,
+        ]);
     }
-
 }
--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -15,7 +15,8 @@ final class ServiceController extends AbstractController
             'controller_name' => 'ServiceController',
         ]);
     }
-      #[Route('/service/{name}', name: 'show_service')]
+
+    #[Route('/service/{name}', name: 'show_service')]
     public function showService(string $name): Response
     {
         return $this->render('service/show.html.twig', [
@@ -23,13 +24,16 @@ final class ServiceController extends AbstractController
         ]);
     }
 
-
- #[Route('/goToIndex', name: 'goToIndex')]
+    #[Route('/goToIndex', name: 'goToIndex')]
     public function goToIndex(): Response
     {
-       
-        return $this->render('app_home'); 
-      
-    }
+        // Option 1 : Afficher directement la vue avec les variables nécessaires
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'identifiant' => 999, // Ajout de la variable manquante
+        ]);
 
+        // Option 2 : Redirection vers la route app_home
+        // return $this->redirectToRoute('app_home');
+    }
 }
